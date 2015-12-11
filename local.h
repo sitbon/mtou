43e2b366519f77f91fdbb047c211972f1f6d468f
Phillip Sitbon <phillip.sitbon@intel.com>
2015-12-10 20:26:30 -0600

    initial commit: multicast forwarding operational
    Signed-off-by: Phillip Sitbon <phillip.sitbon@intel.com>

---
+++ local.h
@@ -0,0 +1,90 @@
+#ifndef __LOCAL_H__
+#define __LOCAL_H__
+
+#include <sys/types.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <time.h>
+#include <pthread.h>
+
+#define MAX_DGRAM_LEN 2048
+
+
+typedef bool        u1;
+typedef int8_t      s8;
+typedef uint8_t     u8;
+typedef int16_t     s16;
+typedef uint16_t    u16;
+typedef int32_t     s32;
+typedef uint32_t    u32;
+typedef int64_t     s64;
+typedef uint64_t    u64;
+
+typedef u8          seq_t;
+typedef u8          buf_t;
+
+
+static inline void ts_diff(struct timespec *dst, struct timespec *a, struct timespec *b)
+{
+    if ((b->tv_nsec - a->tv_nsec) < 0) {
+        dst->tv_sec = b->tv_sec - a->tv_sec - 1;
+        dst->tv_nsec = 1000000000 + b->tv_nsec - a->tv_nsec;
+    } else {
+        dst->tv_sec = b->tv_sec - a->tv_sec;
+        dst->tv_nsec = b->tv_nsec - a->tv_nsec;
+    }
+}
+
+static inline void ts_add(struct timespec *dst, struct timespec *a, struct timespec *b)
+{
+    dst->tv_sec = a->tv_sec + b->tv_sec;
+    dst->tv_nsec = a->tv_nsec + b->tv_nsec;
+
+    while (dst->tv_nsec >= 1000000000) {
+        dst->tv_sec++;
+        dst->tv_nsec -= 1000000000;
+    }
+}
+
+static inline void print_hex(FILE *fd, u8 *buf, u32 len, bool space)
+{
+    for (int i = 0; i < len; i++) printf("%02x%s", buf[i], space ? " " : "");
+    if (len) fputc('\n', fd);
+}
+
+static inline void set_thread_prio(bool max)
+{
+    struct sched_param sched = { .sched_priority = sched_get_priority_max(SCHED_FIFO) };
+    if (!max) sched.sched_priority--;
+    sched_setscheduler(0, SCHED_FIFO, &sched);
+    pthread_setschedparam(pthread_self(), SCHED_FIFO, &sched);
+}
+
+static inline u64 htonll(u64 in)
+{
+    union {
+        u64 num;
+        u8  bytes[8];
+    } dst, src = { .num = in };
+
+    for (int i = 0; i < 8; i++)
+        dst.bytes[i] = src.bytes[7-i];
+
+    return src.num;
+}
+
+static inline u64 ntohll(u64 in)
+{
+    union {
+        u64 num;
+        u8  bytes[8];
+    } dst, src = { .num = in };
+
+    for (int i = 0; i < 8; i++)
+        dst.bytes[i] = src.bytes[7-i];
+
+    return dst.num;
+}
+
+#endif
---
+++ main.c
@@ -0,0 +1,329 @@
+#include "local.h"
+#include "mtou.yucc"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <pthread.h>
+#include <time.h>
+#include <sys/prctl.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <ifaddrs.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#define MAX_QUEUE       16
+
+typedef struct {
+    size_t len;
+    buf_t buf[];
+} msg_t;
+
+typedef struct {
+    msg_t *msg[MAX_QUEUE];
+    size_t cnt;
+    pthread_mutex_t lock;
+    pthread_cond_t can_produce;
+    pthread_cond_t can_consume;
+    pthread_t thr_prod;
+    pthread_t thr_cons;
+} queue_t;
+
+typedef struct {
+    int fd_mc;
+    int fd_uc;
+    struct sockaddr_in addr_out;
+    bool verbose;
+} param_t;
+
+static int udp_addr(struct addrinfo **addr, const char *host, const char *service, bool passive);
+
+static void start(param_t *param);
+static void stop(void);
+static void *rx_thread(void *param);
+static void *tx_thread(void *param);
+
+
+static queue_t queue = {
+        .cnt = 0,
+        .lock = PTHREAD_MUTEX_INITIALIZER,
+        .can_produce = PTHREAD_COND_INITIALIZER,
+        .can_consume = PTHREAD_COND_INITIALIZER
+};
+
+
+int main(int argc, char *argv[])
+{
+    yuck_t argp[1];
+    struct addrinfo *addr_info = NULL;
+    struct sockaddr_in addr_bind;
+    struct sockaddr_in addr_mcast;
+    struct sockaddr_in addr_iface;
+    struct sockaddr_in addr_out;
+    struct ip_mreq mreq;
+    param_t param;
+    int fd_mc, fd_uc, opt;
+
+    yuck_parse(argp, argc, argv);
+
+    if (argp->iface_arg && !strlen(argp->iface_arg)) {
+        fputs("interface is required\n", stderr);
+        exit(1);
+    }
+
+    if (!argp->in_arg || !strlen(argp->in_arg)) {
+        fputs("input is required\n", stderr);
+        exit(1);
+    }
+
+    if (!argp->port_arg || !strlen(argp->port_arg)) {
+        fputs("port is required\n", stderr);
+        exit(1);
+    }
+
+    if (!argp->out_arg || !strlen(argp->out_arg)) {
+        fputs("output is required\n", stderr);
+        exit(1);
+    }
+
+    if (!argp->out_port_arg || !strlen(argp->out_port_arg)) {
+        argp->out_port_arg = argp->port_arg;
+    }
+
+    //printf("%s -> %s :: %s\n", argp->in_arg, argp->out_arg, argp->port_arg);
+
+    if (udp_addr(&addr_info, argp->out_arg, argp->out_port_arg, false)) {
+        exit(1);
+    }
+
+    addr_out = *(struct sockaddr_in *)addr_info->ai_addr;
+
+    if (udp_addr(&addr_info, argp->in_arg, argp->port_arg, false)) {
+        exit(1);
+    }
+
+    addr_bind = addr_mcast = *(struct sockaddr_in *)addr_info->ai_addr;
+
+    if (udp_addr(&addr_info, "0.0.0.0", argp->port_arg, false)) {
+        exit(1);
+    }
+
+    addr_iface = *(struct sockaddr_in *)addr_info->ai_addr;
+
+    if (argp->iface_arg) {
+        struct ifaddrs *addr;
+
+        if (getifaddrs(&addr)) {
+            perror("getifaddrs()");
+            exit(1);
+        }
+
+        while (addr) {
+            if (!addr->ifa_addr || addr->ifa_addr->sa_family != AF_INET) goto next;
+            if (strcmp(argp->iface_arg, addr->ifa_name)) goto next;
+
+            const in_addr_t in_addr = ntohl(((struct sockaddr_in *)addr->ifa_addr)->sin_addr.s_addr);
+            if (in_addr == INADDR_LOOPBACK || in_addr == INADDR_NONE || in_addr == INADDR_ANY) goto next;
+
+            addr_iface.sin_addr = ((struct sockaddr_in *)addr->ifa_addr)->sin_addr;
+            break;
+
+            next:
+            addr = addr->ifa_next;
+        }
+
+        if (!addr) {
+            fputs("no suitable interfaces found", stderr);
+            exit(1);
+        }
+    }
+
+    char addr_iface_str[64], addr_mcast_str[64], addr_out_str[64];
+    strcpy(addr_iface_str, inet_ntoa(addr_iface.sin_addr));
+    strcpy(addr_mcast_str, inet_ntoa(addr_mcast.sin_addr));
+    strcpy(addr_out_str, inet_ntoa(addr_out.sin_addr));
+
+    printf("<%s> %s:%u -> %s:%u\n",
+            addr_iface_str, addr_mcast_str, ntohs(addr_mcast.sin_port),
+            addr_out_str, ntohs(addr_out.sin_port)
+    );
+
+    if ((fd_mc = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    if ((fd_uc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    opt = 1;
+    setsockopt(fd_mc, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    setsockopt(fd_mc, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt));
+
+    if (bind(fd_mc, (struct sockaddr *)&addr_bind, sizeof(addr_bind))) {
+        perror("bind");
+        exit(1);
+    }
+
+    mreq.imr_interface = addr_iface.sin_addr;
+    mreq.imr_multiaddr = addr_mcast.sin_addr;
+
+    if (setsockopt(fd_mc, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq))) {
+        perror("setsockopt");
+        exit(1);
+    }
+
+    param.fd_mc = fd_mc;
+    param.fd_uc = fd_uc;
+    param.addr_out = addr_out;
+    param.verbose = argp->verbose_flag > 0;
+
+    yuck_free(argp);
+    start(&param);
+    while (getchar() != '\n');
+    stop();
+    return 0;
+}
+
+static void start(param_t *param)
+{
+    pthread_create(&queue.thr_prod, NULL, rx_thread, param);
+    pthread_create(&queue.thr_cons, NULL, tx_thread, param);
+}
+
+static void stop(void)
+{
+    if (queue.thr_prod) {
+        pthread_cancel(queue.thr_prod);
+        pthread_join(queue.thr_prod, NULL);
+    }
+
+    if (queue.thr_cons) {
+        pthread_cancel(queue.thr_cons);
+        pthread_join(queue.thr_cons, NULL);
+    }
+}
+
+static void *rx_thread(void *param)
+{
+    const param_t *const p = (param_t *)param;
+    const int fd = p->fd_mc;
+    const bool verbose = p->verbose;
+    msg_t *qmsg;
+    buf_t buf[MAX_DGRAM_LEN];
+    struct sockaddr_in src;
+    socklen_t src_len;
+    ssize_t len;
+
+    set_thread_prio(true);
+
+    while (true) {
+        src_len = sizeof(src);
+        len = recvfrom(fd, buf, MAX_DGRAM_LEN, MSG_NOSIGNAL | MSG_WAITALL, (struct sockaddr *)&src, &src_len);
+
+        if (len <= 0) {
+            if (errno == ENOTCONN) {
+                sleep(1);
+                continue;
+            }
+
+            if (errno) perror("recvfrom");
+            else fprintf(stderr, "mcast rx: len=%li\n", len);
+            sleep(1);
+            continue;
+        }
+
+        if (verbose) {
+            if (src_len >= sizeof(src)) printf("[%s:%u] ", inet_ntoa(src.sin_addr), src.sin_port);
+            print_hex(stdout, buf, (u32)len, false);
+        }
+
+        qmsg = malloc(sizeof(msg_t) + len);
+        qmsg->len = (size_t)len;
+        memcpy(qmsg->buf, buf, qmsg->len);
+        pthread_mutex_lock(&queue.lock);
+
+        while (queue.cnt >= MAX_QUEUE)
+            pthread_cond_wait(&queue.can_produce, &queue.lock);
+
+        queue.msg[queue.cnt] = qmsg;
+        queue.cnt++;
+
+        pthread_cond_signal(&queue.can_consume);
+        pthread_mutex_unlock(&queue.lock);
+    }
+
+    return NULL;
+}
+
+static void *tx_thread(void *param)
+{
+    const param_t *const p = (param_t *)param;
+    const int fd = p->fd_mc;
+    const struct sockaddr_in *const addr = &p->addr_out;
+    msg_t *qmsg;
+    ssize_t len;
+
+    set_thread_prio(false);
+
+    while (true) {
+        pthread_mutex_lock(&queue.lock);
+
+        while (!queue.cnt)
+            pthread_cond_wait(&queue.can_consume, &queue.lock);
+
+        qmsg = queue.msg[--queue.cnt];
+        queue.msg[queue.cnt + 1] = NULL;
+        pthread_cond_signal(&queue.can_produce);
+        pthread_mutex_unlock(&queue.lock);
+
+        if (!qmsg) {
+            fprintf(stderr, "udp tx: msg == NULL\n");
+            continue;
+        }
+
+        send:
+        len = sendto(fd, qmsg->buf, qmsg->len, MSG_WAITALL | MSG_NOSIGNAL, (struct sockaddr *)addr, sizeof(*addr));
+
+        if (len != qmsg->len) {
+            if (len < 0 && errno == EINTR) goto send;
+            if (errno) perror("sendto");
+            else fprintf(stderr, "udp tx: len=%li exp=%li\n", len, qmsg->len);
+        }
+
+        free(qmsg);
+    }
+
+    return NULL;
+}
+
+static int udp_addr(struct addrinfo **addr, const char *host, const char *service, bool passive)
+{
+    struct addrinfo hints = {
+            .ai_family = AF_INET,
+            .ai_socktype = SOCK_DGRAM,
+            .ai_protocol = IPPROTO_UDP,
+            .ai_flags = AI_ADDRCONFIG
+    };
+    
+    if (passive) hints.ai_flags |= AI_PASSIVE;
+
+    const int err = getaddrinfo(host, service, &hints, addr);
+
+    if (err) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(err));
+    }
+
+    return err;
+}
